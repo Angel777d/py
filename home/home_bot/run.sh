8dc4f1a968ae789dd58435f3ceac4634cd5c25d5
Dmitry Emelyanov <angel777da@gmail.com>
2019-08-18 02:20:09 +0300

    run script upgraded

--- home/home_bot/run.sh
+++ home/home_bot/run.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-nohup python3 run.py &
-echo "home_bot started pid" $!
+
+# script_name=$0
+run_option=$1
+
+
+case ${run_option} in
+
+setup)
+echo "setup python env for home bot"
+python3 -m venv env
+echo "env created"
+source ./env/bin/activate
+echo "env activated"
+python3 -m pip install -r requirements.txt
+echo "modules from requirements.txt installed"
+;;
+
+stop)
+pkill -f run.py
+echo "home_bot stopped"
+;;
+
+*) # default case
+echo "start bot"
+# nohup python3 run.py &
+# echo "home_bot started pid" $!
+
+esac
+
 exit 0
